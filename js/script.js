1f9fc602f1a68411e4d57a66790fdd578b0b7a10
Erdinch <erdinchorhan02@gmail.com>
2022-07-26 16:34:09 +0300

    fully working plus and minus operator calc

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ let displayValue = '';
 let operator = '';
 let sum = '';
 let temp = '';
+let tempOperator = '';
 
 function clearEverything() {
   displayValue = '';
@@ -26,34 +27,58 @@ function updateDisplay(selfBtn) {
 
 function operate(selfBtn) {
   operator = selfBtn;
-  temp = +displayValue;
-  displayValue = '';
-  sum = '';
+  temp = +temp;
+  displayValue = +displayValue;
+  sum = +sum;
+
+  if (operator === '+') {
+    if (temp) {
+      if (tempOperator === '-') {
+        temp = temp - displayValue;
+      } else {
+        temp = temp + displayValue;
+      }
+      displayValue = 0;
+      display.value = temp;
+      tempOperator = '+';
+    } else {
+      temp = displayValue;
+      displayValue = 0;
+      tempOperator = '+';
+    }
+  } else if (operator === '-') {
+    if (temp) {
+      if (tempOperator === '+') {
+        temp = temp + displayValue;
+      } else {
+        temp = temp - displayValue;
+      }
+      displayValue = 0;
+      display.value = temp;
+      tempOperator = '-';
+    } else {
+      temp = displayValue;
+      displayValue = 0;
+      tempOperator = '-';
+    }
+  }
 }
 
 function equalsSum() {
   temp = +temp;
   displayValue = +displayValue;
-
-  switch (operator) {
-    case '+':
-      sum = temp + displayValue;
-      break;
-    case '-':
-      sum = temp - displayValue;
-      break;
-    case 'x':
-      sum = temp * displayValue;
-      break;
-    case '%':
-      sum = temp / displayValue;
-      break;
-    default:
-      sum = displayValue;
+  sum = +sum;
+  if (tempOperator === '+') {
+    sum = temp + displayValue;
+    displayValue = sum;
+    display.value = sum;
+    temp = 0;
+  } else if (tempOperator === '-') {
+    sum = temp - displayValue;
+    displayValue = sum;
+    display.value = sum;
+    temp = 0;
   }
-  display.value = +sum;
-  temp = 0;
-  displayValue = +sum;
 }
 
 numberButtons.forEach((button) => {
